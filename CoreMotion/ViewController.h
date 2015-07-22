f64d1e471b60dbf78db2180e4c85298006aa27a0
Arran Purewal <a.purewal92@gmail.com>
2015-07-22 19:45:58 +0100

    Initial Commit

---
+++ CoreMotion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreMotion
+//
+//  Created by Arran Purewal on 22/07/2015.
+//  Copyright (c) 2015 Arran Purewal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreMotion/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreMotion
+//
+//  Created by Arran Purewal on 22/07/2015.
+//  Copyright (c) 2015 Arran Purewal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
